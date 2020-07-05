666d480e00a5b96df371ee05536e6a0f21d40cd5
Chanafin <58449415+chanafin@users.noreply.github.com>
2020-07-05 13:59:10 -0400

    Sunday Edits

---
+++ Older/config_dart.py
@@ -0,0 +1,12 @@
+sf_username = 'kwame6@dartanion.net'
+sf_password = 'Password1!'
+sf_security_token = 'J1tr2UKsuI3rNf6miOjbhObvR'
+
+remote_db_endpoint = 'codingbootcamp.cze9dquabwlh.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'root'
+remote_db_pwd = 'Dartlings!'
+remote_db_name = 'gwsis'
+
+
+
---
+++ config.py
@@ -0,0 +1,16 @@
+# Local Postgres DB Info
+local_db_user = 'postgres'
+local_db_pwd = 'thelog89'
+local_db_endpoint = 'localhost'
+local_db_port = '5432'
+local_db_name = 'gwsis'
+# Cloud/AWS MySQL DB Info
+remote_db_endpoint = 'chanafingw.c7biawpsnxhf.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Seamus20'
+remote_db_name = 'gwsis'
+#Salesforce
+sf_username = 'chrishanafin@gmail.com'
+sf_password = 'Seamus20'
+sf_security_token = 'Qg4fTQj8y1Ir7Xmiz4fCwlmih'
